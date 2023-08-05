8860bed96b9726774ebb365eab3b12ec0444d363
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:03:07 +0000

    Backup Commit

--- reactapp/src/App.test.js
+++ reactapp/src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-15';
 
 test('renders learn react link', () => {
   render(<App />);
--- reactapp/src/components/UI/Card/Card.jsx
+++ reactapp/src/components/UI/Card/Card.jsx
@@ -2,6 +2,7 @@ import '../../../index.css';
 import Button from '../Button/Button'
 import React from 'react';
 
+
 class Card extends React.Component {
     constructor(props) {
         super(props);
